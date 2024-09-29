dda71921718cae6c633b62087b6d0380b06979c1
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-29 19:27:40 +0600

    added default parametere

---
+++ default.js
@@ -0,0 +1,11 @@
+function add(num1=0, num2=0){
+    const result = num1+num2;
+    console.log(num1,num2)
+    return result;
+}
+// const sum = add(4,5)
+// const sum = add(5)
+// const sum = add()
+const sum = add(4)
+
+console.log(sum)
