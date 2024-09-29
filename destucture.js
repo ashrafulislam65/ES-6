6abad882691b324cb1b89e81b7f8715ea4baade8
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-29 21:26:10 +0600

    added destucture

---
+++ destucture.js
@@ -0,0 +1,14 @@
+const obj = {
+    name : 'Rafi',
+    age : 22
+}
+const {name}=obj
+const {age: boyos}=obj///by alias
+// console.log(name)
+// console.log(name)
+// console.log(boyos)
+
+///array
+const num = [2,5,6,8]
+const [first]=num;
+console.log(first)
