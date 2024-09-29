acb3298a8e5784c60f548ceb29d56387a9733564
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-29 20:24:33 +0600

    added arrow function

---
+++ arrow-function.js
@@ -0,0 +1,16 @@
+// function declaration
+function add (a,b){
+   return a+b;
+
+}
+// console.log(add(4,5))
+// function Experassion
+// const sum = function(){
+//   console.log('Rafi')
+// }
+
+
+// Arrow function
+const addition = (a,b)=>a+b;
+const ad = addition(4,7);
+console.log(ad)
