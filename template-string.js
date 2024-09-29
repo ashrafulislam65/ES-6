ea50869646e2b6d7fc8b8b9b9a9df2411e23f6d0
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-29 19:55:49 +0600

    added template string

---
+++ template-string.js
@@ -0,0 +1,10 @@
+const a = 10;
+const b=30;
+// const sum = `Result of a ${a} + b ${b} is ${a+b}`
+const sum = `Result of a ${a} 
++ b ${b} 
+ is ${a+b}`
+console.log(sum)
+// Advantage : 
+// 1.use on multiline string
+// 2.string interpolation
