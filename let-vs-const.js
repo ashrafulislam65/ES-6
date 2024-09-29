a9407f7eb97b19d854aaa55193d01e1ced172921
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-29 19:09:32 +0600

    added es-6 practice task let vs const

---
+++ let-vs-const.js
@@ -0,0 +1,21 @@
+// let num = 60;
+// num=80;
+// console.log(num);
+// const arr = [10,20,30,6];
+// arr[2] = 60;
+// arr.push(3,90,100)
+// console.log(arr);
+
+// let obj ={
+//     name : "Rafi",
+//     age : 22,
+// }
+// obj = {name : 'Rafi'}
+// console.log(obj)
+
+
+
+const num = [0,2,5,6];
+for(let i=0;i<4;i++){
+    console.log(num[i]);
+}
